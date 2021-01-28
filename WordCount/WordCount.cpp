3c424ff3402362c661d5bdc5a130921aea5b72e9
Collin Li <collinli2022@gmail.com>
2021-01-28 19:57:00 +0000

    Added Linked List Class

--- WordCount/WordCount.cpp
+++ WordCount/WordCount.cpp
@@ -7,6 +7,83 @@
 #include <fstream>
 using namespace std;
 
+class ListNode {
+	private:
+		string value;
+		ListNode* next;
+    int appear;
+	public:
+		ListNode(string a="", ListNode* b=NULL);
+		
+		//adds word to Linked List and if there is duplicate then add to number of times repeated
+		ListNode* add(ListNode* head, string word);
+
+		// Getters
+		string getValue() { return value; }
+		ListNode* getNext() { return next; }
+
+    // Setters
+    void addAppear(int add=1);
+		
+		~ListNode() { // deconstructor that deletes pointer
+      next = nullptr;
+      delete next;
+      } 
+};
+
+ListNode::ListNode(string a, ListNode* b) { // sets string and pointer
+	value = a; 
+	next = b;
+  appear = 1;
+}
+
+ListNode* ListNode::add(ListNode* head, string word) {
+  // add word to linked list
+
+  // find alphabetical position
+  ListNode* pos = head;
+  ListNode* before = NULL;
+
+  // 0 - means need to add new Node to end
+  // 1 - means found node with same string word
+  // 2 - means need to add node at beginning or in middle
+  int operation = 0; 
+  while(pos != NULL) { // iterate through Linked List to find right index
+    if(pos.getValue() == word) {
+      pos.addAppear();
+      operation = 1;
+      break;
+    } else if(word > pos.getValue()) { // keep going down list
+      before = pos;
+      pos = pos->next;
+    } else {
+      
+      break;
+    }
+  }
+
+  if(operation == 0) {
+
+  } else if(operation == 2) {
+    if(before != NULL) { // need to add new node in middle
+        ListNode* newNode = new ListNode(word, before->next);
+        before->next = newNode;
+    } else { // need to add new node in beginning
+
+    }
+  }
+
+  return head;
+  
+
+  
+
+}
+
+void ListNode::addAppear(int add) {
+  appear += add;
+}
+
 /*
 Write a program called WordCount that first prompts the user for the name of a file and then performs a count of the number of times a word appears in the file. There will be no punctuation, but there can be numbers. Assume words are not case-sensitive here (in other words, "HeLlO" and "hello" are the same word). At the end of the program, display the list of words in alphabetical order, one word and its count per line, in a new text file that is named "result.txt" (and is created by your program). Your program MUST use Linked Lists to store the words. Standard rules apply for formatting and usability. 4 points for documentation, 4 points for user-friendliness, 22 points for the required functionality (file read, linked list, file write, words properly sorted and counted).
 */
@@ -23,8 +100,6 @@ int main () {
   cout << "Enter word to search: ";
   string find;
   cin >> find;
-
-  ASK MRS.ZINN can i first search for the word without linked list then store into linked list
 
   string line;
   ifstream myfile (fileName);
