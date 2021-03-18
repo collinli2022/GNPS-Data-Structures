a85206d6547a7361e630117df6d1a87377c2fb0e
Collin Li <collinli2022@gmail.com>
2021-03-18 23:08:41 +0000

    some fixes but need to implement methods

--- ParenthesesMatch/ParenthesesMatch.cpp
+++ ParenthesesMatch/ParenthesesMatch.cpp
@@ -11,7 +11,6 @@
 using namespace std;
 class Stack {
   private:
-    Stack* previous;
     Stack* next;
     string value;
   public:
@@ -19,61 +18,105 @@ class Stack {
     string RIGHT = ")]}>";
     bool checkParen(string s);
 
-    Stack(string a, Stack* b=NULL, Stack* n=NULL); // constructor
+    Stack(string a="", Stack* n=NULL); // constructor
 
-    Stack* add(string a, Stack* head); // add to top of stack
+    Stack* add(string a); // add to top of stack
 
-    string pop(Stack* head); // get head and remove head
+    string pop(); // get head and remove head
 
     // Getters
     string getValue() { return value; }
     Stack* getNext() { return next; }
-    Stack* getPrevious() { return previous; }
     
     ~Stack() { delete next; } // deconstructor that deletes pointer
 };
 
-class ParenMatch
-{
+Stack::Stack(string a, Stack* n) { // sets string and pointers
+  value = a;
+  next = n;
+}
+
+Stack* Stack::add(string a) {
+  if(this->value=="") {
+    cout << "this is null2" << endl;
+    return new Stack(a, NULL);
+  }
+  Stack* newHead = new Stack(a, this); // next is previous head
+  return newHead;
+}
+
+string Stack::pop() {
+  string returnStr = "";
+  if(this->value == "") {
+    return "";
+  } else if(this->next == NULL) {
+    returnStr = this->value;
+    this->value.clear();
+    return returnStr;
+  }
+  returnStr = this->value;
+  Stack* newHead = this->next; // new head
+  this->next = newHead->next;
+  this->value = newHead->value;
+  return returnStr;
+}
+
+class ParenMatch {
   public:
     string LEFT = "([{<";
     string RIGHT = ")]}>";
   
+    bool checkParen(string input) {
+      Stack* stack = new Stack();
+      // Traverse the string 
+      for (int i = 0; i < input.length(); i++) {
+        if (LEFT.find(input[i]) != string::npos) { 
+          cout << "found LEFT: " << input[i] << endl;
+          string temp(input[i], 1);
+          stack = stack->add(temp);
+        } else if (RIGHT.find(input[i]) != string::npos) { 
+          int index1 = RIGHT.find(input[i]);
+          int index2 = LEFT.find(stack->pop());
+          if(index1 != index2) { return false; }
+        }
+      }
+      return true;
+    }
+
     ~ParenMatch() {} // deconstructor that deletes pointer
 };
 
-
-Stack::Stack(string a, Stack* b, Stack* n) { // sets string and pointers
-  value = a;
-  previous = b; 
-  next = n;
+// debugging purposes
+void printMe(Stack* head) {
+	cout << "[";
+	while(head != NULL)	{
+		 cout << head->getValue();
+		 head = head->getNext();
+		 if(head != NULL)
+			 cout << ", ";
+	}
+	cout << "]" << endl;
 }
-
-Stack* Stack::add(string a, Stack* head) {
-  Stack* newHead = new Stack(a, NULL, head); // next is previous head
-  head->previous = newHead;
-  return newHead;
-}
-
-string Stack::pop(Stack* head) {
-  if(head == NULL) {
-    return NULL;
-  }
-  string returnStr = head->value;
-  head->next->previous = NULL;
-  Stack* newHead = head->next;
-  head->next = NULL;
-  head = nullptr;
-  head = newHead;
-  return returnStr;
-}
-Ask mrs zinn how to return both the pop output and set head to be new head
 
 /*
 Write a program called ParenthesesMatch that first prompts the user for the name of a file and then checks to see whether or not the parentheses for each line match. The txt file will contain a series of runs of the program, where each line consists of mathematical expressions involving the bracket symbols. Your program must output the trial number followed by a colon and a single space and then the result of the call to checkParen, one trial per line, in a new text file called "result.txt" that is created by your program. Your program MUST use a stack to run each trial. Standard rules apply for formatting and usability. 4 points for documentation, 4 points for user-friendliness, 24 points for the required functionality (file read, stack, file write, correct result for each trial).
 */
 
 int main() {
+  // testing
+  Stack* head = new Stack();
+  head = head->add("bob");
+  head = head->add("no");
+  
+  printMe(head);
+  
+  cout << "3" << endl;
+  cout << head->pop() << endl;
+  cout << "4" << endl;
+  cout << head->pop() << endl;
+  cout << head->pop() << endl;
+  
+  
   // prompts the user for the name of a file
   cout << "Enter the name of the file: ";
   string inputName;
