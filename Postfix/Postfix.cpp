0adcfa498691c380e5b9ea6a8afd1f3f0df4302e
Collin Li <collinli2022@gmail.com>
2021-03-24 23:58:44 +0000

    mad cuz bad

--- Postfix/Postfix.cpp
+++ Postfix/Postfix.cpp
@@ -89,8 +89,7 @@ class Postfix {
           int A = stack->pop();
           int B = stack->pop();
           stack = stack->push(A + B);
-        }
-        else if (temp.compare("-") == 0) { //Checks for subtraction
+        } else if (temp.compare("-") == 0) { //Checks for subtraction
           int A = stack->pop();
           int B = stack->pop();
           stack = stack->push(B - A);
@@ -101,7 +100,7 @@ class Postfix {
         } else if (temp.compare("/") == 0) { //Checks for division
           int A = stack->pop();
           int B = stack->pop();
-          stack = stack->push(B / A);
+          stack = stack->push( (int)(B / A));
         } else if (temp.compare("%") == 0) { //Checks for modulus
           int A = stack->pop();
           int B = stack->pop();
@@ -118,7 +117,6 @@ class Postfix {
           int A = stack->pop();
           int result = 1;
           for (int i = 1; i <= A; i++) { result *= i; }
-          stack = stack->push(result);
         }
         else { // Not an operator so a number
           stack = stack->push(stoi(temp));
@@ -144,6 +142,8 @@ int main() {
   if (fileInput.is_open()) { // open file
     fileOutput.open("result.txt");
     while ( getline (fileInput, line) ) { // get lines
+      cout << trialIndex << ": " << line << endl;
+
       std::stringstream iss( line );
       string number;
       std::vector<string> myNumbers;
@@ -153,7 +153,7 @@ int main() {
       string output = postfix.simulate(myNumbers); // see if grouping of parenthesis is correct
 
       fileOutput << line << ": " << output << "\n";
-
+      
       trialIndex += 1;
     }
     fileInput.close();
