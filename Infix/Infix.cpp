44f9dcdbc701a312cce0ae06f1687b3104e4383a
Collin Li <>
2021-04-10 00:40:27 +0000

    done done

--- Infix/Infix.cpp
+++ Infix/Infix.cpp
@@ -15,15 +15,15 @@ using namespace std;
 class Stack {
   private:
     Stack* next;
-    char value; // Stores the value
+    int value; // Stores the value
   public:
     bool checkParen(string s);
 
-    Stack(char a='.', Stack* n=NULL); // constructor
+    Stack(int a=INT_MAX, Stack* n=NULL); // constructor
 
-    Stack* push(char a); // add to top of stack
+    Stack* push(int a); // add to top of stack
 
-    char pop(); // get head and remove head
+    int pop(); // get head and remove head
 
     // debugging purposes
     void printMe(Stack* head) {
@@ -38,42 +38,42 @@ class Stack {
     }
 
     // Getters
-    char getValue() { return value; }
+    int getValue() { return value; }
     Stack* getNext() { return next; }
     
     ~Stack() { delete next; } // deconstructor that deletes pointer
 };
 
-Stack::Stack(char a, Stack* n) { // sets int and pointers
+Stack::Stack(int a, Stack* n) { // sets int and pointers
   value = a;
   next = n;
 }
 
-Stack* Stack::push(char a) {
-  if(this->value=='.') { // means the stack is empty
+Stack* Stack::push(int a) {
+  if(this->value==INT_MAX) { // means the stack is empty
     return new Stack(a, NULL);
   }
   Stack* newHead = new Stack(a, this); // sets this node to next node and replaces head with new node
   return newHead;
 }
 
-char Stack::pop() {
-  char returnChr = '.';
-  if(this->value == '.') { // means stack is empty
+int Stack::pop() {
+  int returnStr = INT_MAX;
+  if(this->value == INT_MAX) { // means stack is empty
     return 0;
   } else if(this->next == NULL) { // means that stack has length of 1
-    returnChr = this->value;
-    this->value = '.';
-    return returnChr;
+    returnStr = this->value;
+    this->value = INT_MAX;
+    return returnStr;
   }
-  returnChr = this->value;
+  returnStr = this->value;
   Stack* newHead = this->next; // new head
   this->next = newHead->next; // set old head to new head
   this->value = newHead->value;
-  newHead->value = '.'; // delete next node since *this* head has taken it's spot
+  newHead->value = INT_MAX; // delete next node since *this* head has taken it's spot
   newHead->next = nullptr;
   delete newHead;
-  return returnChr;
+  return returnStr;
 }
 
 class Infix {
@@ -93,49 +93,77 @@ class Infix {
       string result = "";
       Stack* st = new Stack();
       st = st->push('N');
-
       vector<string> s;
-      while ( iss >> infix ) {
-        myNumbers.push_back( number );
-      }
-
-      for(int i=0; i < myNumbers.size(); i++){
-        cout << s[i] << endl;
-        if(s[i] == ' ') {} // Do nothing if it is a space
-        else if((s[i] >= '0' && s[i] <= '9')) { result += s[i]; result += " "; } // If the index char is an operand, add it to output string.
-        else if(s[i] == '(') { st = st->push('('); } // If the scanned character is an '(', push it to the stack.
-        else if(s[i] == ')') { // If the index char is an ')', pop and to output string from the stack until an '(' appears.
+      stringstream iss(infix);
+      string temp;
+      while ( iss >> temp ) { s.push_back(temp); }
+      for(int i=0; i < s.size(); i++) {
+        /* // Debug Purposes
+        cout << "index: " << s[i] << endl;
+        st->printMe(st);
+        cout << endl << endl;
+        */
+        if(s[i] == " ") {} // Do nothing if it is a space
+        else if((s[i].back() >= '0' && s[i].back() <= '9')) { result += s[i]; result += " "; } // If the index char is an operand, add it to output string.
+        else if(s[i].back() == '(') { st = st->push('('); } // If the scanned character is an '(', push it to the stack.
+        else if(s[i].back() == ')') { // If the index char is an ')', pop and to output string from the stack until an '(' appears.
           while(st->getValue() != 'N' && st->getValue() != '(') {
-            char c = st->pop();              
+            char c = (char)st->pop();              
             result += c;
             result += " ";
           }
           if(st->getValue() == '(') {
-              char c = st->getValue();
-              st->pop();
+              char c = (char) st->pop();
           }
         } else { // If an operator is at index
-            while(st->getValue() != 'N' && isLower(s[i], st->getValue())) {
-                char c = st->pop();
+            while(st->getValue() != 'N' && isLower(s[i].back(), st->getValue()) && st->getValue() != '(') {
+                char c = (char)st->pop();
                 result += c;
                 result += " ";
             }
-            st = st->push(s[i]);
-            cout << s[i] << " - " << endl;
-            st->printMe(st);
-        }
-        cout << "adf: " << result << endl;
-        st->printMe(st);
+            st = st->push(s[i].back());
+        } 
       }
-
-      cout << "DFK: " << result << endl;
-      
-      // Pop all the remaining elements from the stack
-      while(st->getValue() != 'N') {
-          char c = st->pop();
+      while(st->getValue() != 'N') { // Pop all the remaining elements from the stack
+          char c = (char)st->pop();
           result += c;
+          result += " ";
       }
       return result;    
+    }
+
+    int eval(string postfix) { // does postfix
+      stringstream iss(postfix);
+      string number;
+      vector<string> tokens;
+      while ( iss >> number ) {
+        tokens.push_back( number );
+      }
+
+      Stack* stack = new Stack(); // Creates a stack which will be responsible for the postfix operations
+      for (int i = 0; i < tokens.size(); i++) {
+        string temp = tokens.at(i); // Gets the token in question
+        if (temp.compare("+") == 0) { // Checks for addition
+          int A = stack->pop();
+          int B = stack->pop();
+          stack = stack->push(A + B);
+        } else if (temp.compare("-") == 0) { // Checks for subtraction
+          int A = stack->pop();
+          int B = stack->pop();
+          stack = stack->push(B - A);
+        } else if (temp.compare("*") == 0) { // Checks for multiplication
+          int A = stack->pop();
+          int B = stack->pop();
+          stack = stack->push(A * B);
+        } else if (temp.compare("/") == 0) { // Checks for division
+          int A = stack->pop();
+          int B = stack->pop();
+          stack = stack->push((int)(B / A));
+        } else { // Not an operator so a number
+          stack = stack->push(stoi(temp));
+        }
+      }
+      return stack->pop(); // Returns the final leftover item in the stack, which is the result of the operations
     }
 
     //returns true if c1 has lower or equal precedence than c2
@@ -145,13 +173,7 @@ class Infix {
 };
 
 int main() {
-  
-
   Infix infix = Infix(); // used to solve
-
-  // TESTING
-  cout << "Start" << endl;
-  cout << infix.infixToPostfix("3 * ( 4 * 5 -6+2)") << endl;
 
   // prompts the user for the name of a file
   cout << "Enter the name of the file: ";
@@ -164,11 +186,11 @@ int main() {
   int trialIndex = 0; // count trial number
   if (fileInput.is_open()) { // open file
     fileOutput.open("result.txt");
-    fileOutput << "Infix   -->   Postfix   -->   Evaluate\n";
+    // fileOutput << "Infix    -->    Postfix    -->    Evaluate\n";
     while ( getline (fileInput, line) ) { // get lines
-      // cout << trialIndex << ": " << line << endl; // debug
-      string output = infix.infixToPostfix(line);
-      fileOutput << line << "   " << output << "   " << "blah" << endl; // outputs to txt
+      string outputInfix = infix.infixToPostfix(line);
+      int outputPostfix = infix.eval(outputInfix);
+      fileOutput << line << "\t\t\t\t" << outputInfix << "\t\t\t\t" << outputPostfix << endl; // outputs to txt
       trialIndex += 1;
     }
     fileInput.close();
