60df6c0510a844586b7fd3e7e4340b47bd145233
Collin Li <>
2021-04-22 21:17:49 +0000

    haha L

--- AssemblyLine/AssemblyLine.cpp
+++ AssemblyLine/AssemblyLine.cpp
@@ -17,7 +17,7 @@ template <typename T> class Stack { // add to the head and take from head
     T* value;
     Stack* next;
   public:
-    Stack(T* v=NULL, Stack* n=NULL) { value = v; n = next; }
+    Stack(T* v=NULL, Stack* n=NULL) { value = v; next = n; }
 
     T* pop() {
       if(next != NULL) {
@@ -28,9 +28,11 @@ template <typename T> class Stack { // add to the head and take from head
       } else { value = NULL; return value; }
       return NULL;
     }
+    T* peek() { return value; }
+    bool isEmpty() {return (value==NULL); }
 
     void push(T* v) {
-      if(next != NULL) {
+      if(value != NULL) {
         Stack<T>* oldHead = new Stack<T>(value, next);
         value = v;
         next = oldHead;        
@@ -45,12 +47,12 @@ template <typename T> class Stack { // add to the head and take from head
       string returnS = "[";
       Stack* node = this;
       while(node != NULL) {
-        returnS += node->value->getValue();
+        returnS += to_string(node->value->getValue());
         returnS += ", ";
         node = node->next;
       }
       if(returnS.length()>2) {
-        returnS=returnS.substr(returnS.length()-2);
+        returnS=returnS.substr(0,returnS.length()-2);
       }
       returnS+= "]";
       return returnS;
@@ -65,7 +67,7 @@ template <class T> class Queue { // add to tail and take from head
   public:
     Queue(T* v=NULL, Queue* n=NULL) {value = v; next = n; } // Constructor
 
-    T pop() { // take from head
+    T* pop() { // take from head
       if(next != NULL) {
         T* returnT = value;
         value = next->value;
@@ -73,21 +75,41 @@ template <class T> class Queue { // add to tail and take from head
         return returnT;
       } else { value = NULL; return value; }
     }
+    T* peek() { return value; }
 
     void push(T* v) { // add to tail
-      Queue* last = this;
-      while(last->next != NULL) {
-        last=last->next;
+      if(value==NULL) {
+        value=v;
+        return;
       }
-      if(last != NULL) {
-        last->next=v;        
-      } else { value = v; }
+      Queue<T>* last = this;
+      while(last->next != NULL) { last=last->next; }
+      Queue<T>* newTail = new Queue<T>(v, NULL);
+      last->setNext(newTail);
+    }
+
+    bool isEmpty() {
+      return value == NULL;
     }
 
     void setValue(T v) { value = v; }
     T getValue() { return value; }
     void setNext(Queue* n) { next = n; }
     Queue* getNext() { return next; }
+    string toString() { // only work with disks
+      string returnS = "[";
+      Queue* node = this;
+      while(node != NULL) {
+        returnS += node->value->toString();
+        returnS += ", ";
+        node = node->next;
+      }
+      if(returnS.length()>2) {
+        returnS=returnS.substr(0,returnS.length()-2);
+      }
+      returnS+= "]";
+      return returnS;
+    }
     ~Queue() { delete next; }
 };
 /* --------------------------------------------------templates-------------------------------------------------- */
@@ -98,6 +120,7 @@ class Disk {
     Disk(int r) { radius = r;}
     int getValue() { return radius;}
     void setRadius(int r) { radius = r; }
+    string toString() {return to_string(radius); }
     ~Disk() { }
 };
 
@@ -108,7 +131,12 @@ class Pyramid {
     Pyramid(Stack<Disk>* d=new Stack<Disk>()) { disks = d; }
     
     Disk* pop() { return disks->pop(); }
+    Disk* peek() { return disks->peek(); }
     void push(Disk* value) { disks->push(value); }
+
+    bool isEmpty() {
+      return disks->isEmpty();
+    }
 
     string toString() {
       return disks->toString();
@@ -119,16 +147,37 @@ class Pyramid {
 
 class AssemblyLine {
   private:
-    Queue<Disk>* assemblyLineIn;
-    Queue<Pyramid>* assemblyLineOut;
+    Queue<Disk> assemblyLineIn;
+    Queue<Pyramid> assemblyLineOut;
+    Pyramid robotArm;
   public:
     // initializes this object so the assemblyLineIn contains nDisks with random radii; assemblyLineOut is initialized to an empty Queue; robotArm is initialized to an empty Pyramid.
-    AssemblyLine() {
+    AssemblyLine(string line) {
+      assemblyLineIn = Queue<Disk>();
+      assemblyLineOut = Queue<Pyramid>();
+      robotArm = Pyramid();
+
+      stringstream iss(line);
+      string number;
+      vector<string> myNumbers;
+      while ( iss >> number ) {
+        myNumbers.push_back( number );
+      }
+
+      for(string i:myNumbers) {
+        assemblyLineIn.push(new Disk(stoi(i)));
+      }
+
+      assemblyLineOut = Queue<Pyramid>();
+      robotArm = Pyramid();
     }
     
     // "flips over" the pyramid in the robotArm and adds it to the assemblyLineOut queue. Precondition: robotArm is not empty and holds an inverted  pyramid of disks
     void unloadRobot() {
-
+      Pyramid temp = Pyramid();
+      while(!robotArm.isEmpty()) { temp.push(robotArm.pop()); }
+      assemblyLineOut.push(&temp);
+      robotArm = Pyramid();
     }
 
     /* processes all disks from assemblyLineIn; a disk is processed as follows:
@@ -141,16 +190,23 @@ class AssemblyLine {
                   assemblyLineOut is empty
     */
     void process() {
-
+      while(!assemblyLineIn.isEmpty()) {
+        if (robotArm.isEmpty() || assemblyLineIn.peek()->getValue() > robotArm.peek()->getValue()) {
+          robotArm.push(assemblyLineIn.pop());
+        } else {
+          unloadRobot();
+        }
+      }
     }
-
+    
+    string getAssemblyLineOut() {
+      return assemblyLineOut.toString();
+    }
     ~AssemblyLine() { }
 };
 
 
 int main() {
-
-
   Pyramid temp;
   temp.push(new Disk(5));
   cout << "A: " << temp.toString() << endl;
@@ -159,7 +215,18 @@ int main() {
   temp.push(new Disk(34));
   cout << "B: " << temp.toString() << endl;
   cout << temp.pop() << endl;
-  cout << temp.toString() << endl;
+  cout << temp.toString() << endl << endl;
+
+  Queue<Disk> aa;
+  cout << "Start" << endl;
+  aa.push(new Disk(5));
+  cout << "A: " << aa.toString() << endl;
+  aa.push(new Disk(7));
+  aa.push(new Disk(6));
+  aa.push(new Disk(34));
+  cout << "B: " << aa.toString() << endl;
+  cout << aa.pop() << endl;
+  cout << aa.toString() << endl;
 
   // prompts the user for the name of a file
   cout << "Enter the name of the file: ";
@@ -174,7 +241,10 @@ int main() {
     fileOutput.open("result.txt");
     // fileOutput << "Infix    -->    Postfix    -->    Evaluate\n";
     while ( getline (fileInput, line) ) { // get lines
-      fileOutput << line << endl; // outputs to txt
+      AssemblyLine input = AssemblyLine(line);
+      input.process();
+      string output = input.getAssemblyLineOut();
+      fileOutput << trialIndex << ": " << output << endl; // outputs to txt
       trialIndex += 1;
     }
     fileInput.close();
